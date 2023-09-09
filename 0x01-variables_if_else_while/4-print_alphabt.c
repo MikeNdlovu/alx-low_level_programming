7d8b9343beac91b80126c9cbcc53fd90eace8d7a
MikeNdlovu <mickeymusa03@gmail.com>
2023-09-09 08:46:07 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/** 
+*main - Classifies neg, pos & zero 
+*Return: Always (success)
+*/
+int main(void)
+{
+
+char z;
+
+for (z = 'a', z <= 'z', z++)
+{
+while (z != e, z != q)
+{
+putchar(z);
+}
+
+putchar('\n');
+return (0);
+}
+
