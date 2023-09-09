c49a51b4d5157e8d37e01ec8f2be57c9fea97f6c
MikeNdlovu <mickeymusa03@gmail.com>
2023-09-09 14:28:49 -0700

    sfdsv

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/** 
+*main - Classifies neg, pos & zero 
+*Return: Always (success)
+*/
+int main(void
+{
+
+int d, l, p;
+
+for (d = '0'; d < '9'; d++)
+{
+for (l = d + 1; l <= 9; l++)
+{
+for (p = l + 1; l <= '9'; q++)
+{
+if ((l != d) != p)
+{
+putchar(d);
+putchar(l);
+putchar(p);
+if (d == '7' && l == '8')
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
