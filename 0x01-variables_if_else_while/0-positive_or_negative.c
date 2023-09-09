25680546a948355dae56a585781a137d2bf282f2
MikeNdlovu <mickeymusa03@gmail.com>
2023-09-09 06:24:02 -0700

    C script printing random + and - int

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there 
+*main - Classifies neg, pos & zero 
+*Return: Always (success)
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+        if (n > 0)
+        printf("%d is positve\n", n);
+        
+        else if (n == 0)
+        rintf("%d is zero\n", n);
+        
+        else if (n < 0)
+        printf("%d is negative\n", n);
+        
+	return (0);
+}
+
