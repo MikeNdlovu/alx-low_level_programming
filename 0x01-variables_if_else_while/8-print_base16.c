c66280d7b31da043480aa5dd53c5701fb87b5ae9
MikeNdlovu <mickeymusa03@gmail.com>
2023-09-09 11:21:23 -0700

    print base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - print 1-9 and a-f
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+
+int a;
+char J;
+
+for (a = '0'; a <= '9'; a++)
+{
+putchar(a);
+}
+
+for (J = 'a'; J <= 'f'; J++ )
+{
+putchar(J);
+}
+
+putchar('\n');
+return (0);
+}
