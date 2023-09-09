561bab76dad01dc4fa4a29a49ec0aa60a0d7683d
MikeNdlovu <mickeymusa03@gmail.com>
2023-09-09 06:33:58 -0700

    C

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,27 +1,35 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there 
+#include <math.h>
+/* 
 *main - Classifies neg, pos & zero 
 *Return: Always (success)
 */
 
 int main(void)
 {
-	int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 	
-        if (n > 0)
-        printf("%d is positve\n", n);
-        
-        else if (n == 0)
-        rintf("%d is zero\n", n);
-        
-        else if (n < 0)
-        printf("%d is negative\n", n);
-        
-	return (0);
+if (n > 0)
+{
+printf("%d is positve\n", n);
+}
+
+else if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+
+else if (n < 0)
+{
+printf("%d is negative\n", n);
+}
+
+return (0);
 }
 
