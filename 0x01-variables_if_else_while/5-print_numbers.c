4f65894f26997b1da5294d9f024f8c06e09e1f13
MikeNdlovu <mickeymusa03@gmail.com>
2023-09-09 10:39:45 -0700

    6-print_numberz

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -11,7 +11,7 @@ int main(void)
 
 int w;
 
-for(w = 0; w < 10; w++)
+for (w = 0; w < 10; w++)
 {
 printf("%d", w);
 }
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/** 
+*main - Classifies neg, pos & zero 
+*Return: Always (success)
+*/
+int main(void) 
+
+{
+
+int w;
+
+for (w = '0'; w <= '9'; w++)
+{
+putchar(w);
+}
+putchar('\n');
+return (0);
+}
