d6c0721dad23591df4a34551d344a349482b4c71
MikeNdlovu <mickeymusa03@gmail.com>
2023-09-09 14:08:51 -0700

    sdfvd

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - different number combinations
+*
+* Return: 0 (SUCCESS)
+*/ 
+int main(void)
+{       
+        
+int a;
+int b;
+
+for (a = '0'; a <= '8' ; a++)
+{       
+
+for (b = '1'; b <= '9'; b++)
+{
+if (a != b)
+{
+putchar(a);
+putchar(b);
+if (a == '8' && b == '9')
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
