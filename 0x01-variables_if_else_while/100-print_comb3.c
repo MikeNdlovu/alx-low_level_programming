6f56b9d32a716430a54c74c56f4e109c2fa8b604
MikeNdlovu <mickeymusa03@gmail.com>
2023-09-11 02:17:02 -0700

    10 print combo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,27 +3,31 @@
 #include <time.h>
 
 /**
-* main - different number combinations
-*
-* Return: 0 (SUCCESS)
-*/ 
+ * main - different number combinations
+ *
+ * Return: 0 (SUCCESS)
+*/
 int main(void)
-{       
-        
+{
+
 int a;
 int b;
 
 for (a = '0'; a <= '8' ; a++)
-{       
+{
 for (b = a + 1; b <= '9'; b++)
 {
 if (b != a)
+{
 putchar(a);
 putchar(b);
+}
 if (a == '8' && b == '9')
 continue;
+{
 putchar(',');
 putchar(' ');
+}
 }
 }
 putchar('\n');
