c7b60bd151bc9d959dbc89fb1480254ff7ab76d7
MikeNdlovu <mickeymusa03@gmail.com>
2023-09-10 23:23:51 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,27 +1,21 @@
 #include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+#include <time.h>
 
-/** 
-*main - Classifies neg, pos & zero 
-*Return: Always (success)
+/**
+ * main - print alpha
+ *
+ * Return: Always (SUCCESS)
 */
 int main(void)
-
 {
 
 char n;
 
 for (n = 'a'; n <= 'z'; n++)
 {
-putchar(n);
+putchar(m);
 }
-
-for (n = 'A'; n <= 'Z'; n++)
-{
-putchar(n);
-}
-
 putchar('\n');
 return (0);
 }
