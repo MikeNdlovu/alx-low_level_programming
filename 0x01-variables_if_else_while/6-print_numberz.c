79e5191543f0685d664fe4fd2a6198fcd11d4782
MikeNdlovu <mickeymusa03@gmail.com>
2023-09-10 22:45:25 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,13 +1,13 @@
 #include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+#include <time.h>
 
-/** 
-*main - Classifies neg, pos & zero 
-*Return: Always (success)
+/**
+ * main - print zero - nine
+ *
+ *Return: Always (SUCCESS)
 */
-int main(void) 
-
+int main(void)
 {
 
 int w;
@@ -16,6 +16,7 @@ for (w = '0'; w <= '9'; w++)
 {
 putchar(w);
 }
+
 putchar('\n');
 return (0);
 }
