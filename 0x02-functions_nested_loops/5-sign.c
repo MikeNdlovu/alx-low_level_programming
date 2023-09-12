f1c8a737bdcdc2a45c0adce85997ee3759d496b2
MikeNdlovu <mickeymusa03@gmail.com>
2023-09-12 12:38:44 -0700

    islowerc

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_sign - classifies +,-
+ * @n: number identified
+ * Return: Always (success)
+ */
+
+int print_sign(int n)
+{
+	if (n > '0')
+	{
+	_putchar(n);
+	_putchar(',');
+	_putchar(' ');
+	_putchar('\n')
+	return (1);
+	}
+
+	if (n = '0')
+	{
+	_putchar(n);
+	_putchar(',');
+	_putchar(' ');
+	_putchar('\n');
+	return (0);
+	}
+
+	else
+	{
+	_putchar(n);
+	_putchar(',');
+	_putchar(' ');
+	_putchar('\n');
+	return (-);
+	}
+}
