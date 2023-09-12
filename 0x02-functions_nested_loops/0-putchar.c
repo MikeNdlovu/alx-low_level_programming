ec542993ad787e26d0eff63da6f85edebd9ef8c3
MikeNdlovu <mickeymusa03@gmail.com>
2023-09-12 06:09:33 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<time.h>
+#include <math.h>
+
+/**
+ * main - print putchar
+ *
+ * Return: Zero (SUCCESS)
+*/
+int main(void)
+{
+
+	printf("_putchar\n");
+	return (0);
+}
