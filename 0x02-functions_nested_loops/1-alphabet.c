c2923ad0ccfe3e372ad8d0d895e4f0e7a10b2b20
MikeNdlovu <mickeymusa03@gmail.com>
2023-09-12 08:09:37 -0700

    void print_alphabet(void);

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - _putchar print alpha
+ *
+ * Return: 0 (success)
+*/
+
+void print_alphabet(void);
+{
+
+	char t;
+
+	for (t = 'a'; t <= 'z'; t++)
+	{
+	_putchar(t)
+	}
+	_putchar('\n')
+	return (0)
+}
