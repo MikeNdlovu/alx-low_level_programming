c80f1fa7611232614bef98ae77cee1779edde574
MikeNdlovu <mickeymusa03@gmail.com>
2023-09-12 13:21:48 -0700

    islowerc

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value of int
+ * @n: int to be checked out
+ * Return: Always (success)
+*/
+
+int _abs(int n)
+{
+
+	if (n >= 0)
+	{
+	return (n);
+	}
+	return (-n);
+}
