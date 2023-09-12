5be6e4c6211d3d294cf067d9f1c6eaafd5bb7908
MikeNdlovu <mickeymusa03@gmail.com>
2023-09-12 12:16:44 -0700

    islowerc

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * int _isalpha - checks char or other
+ * @c: variable to be checked
+ * Return: Always (Success) 
+*/
+int _isalpha(void)
+{
+
+	if (c >= 'a' && c <= 'z')
+	return (1);
+
+	else if (c >= 'A' && c <= 'Z')
+	return (1);
+
+	else
+	return (0);
+}
