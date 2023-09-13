27c145a64dcde697807e0d28de3b0916115d5f8c
MikeNdlovu <mickeymusa03@gmail.com>
2023-09-13 13:21:15 -0700

    _putchar('\n');

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,22 +10,34 @@ void print_times_table(int n)
 {
 	int a, b;
 
-	for (a = 0; a <= 12; a++)
+	for (a = 0; a <= 15; a++)
 	{
-	for (b = 0; b <= 12; b++)
+	for (b = 0; b <= 15; b++)
 	{
 	n = a *b;
-	if (a < 1 && b < 4)
+	if (a == 0)
 	{
-	if (a < 1 && b > 2)
+	putchar(n + '0');
+	}
+	else if (n < 10 && a != 0)
 	{
-	printf("%d,   \n", n);
+	printf(",   ");
+	_putchar(n + '0');
 	}
-	if (a > 0 && b < 4)
+	else if (n >= 10 && n < 100)
 	{
-	printf(",   %d\n", n);
+	printf(",  ");
+	_putchar((n / 10) + '0');
+	_putchar((n % 10) + '0');
 	}
+	else if (n >= 100)
+	{
+	printf(", ");
+	_putchar((n / 100) + '0');
+	_putchar(((n / 10) % 10) + '0');
+	_putchar((n % 10) + '0');
 	}
-	}
+	_putchar('\n')
+}
 }
 }
