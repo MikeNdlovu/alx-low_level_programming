47d60ce3c0f0f10711e9b6333aaf0b73a887d8ee
MikeNdlovu <mickeymusa03@gmail.com>
2023-09-14 08:50:59 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiplies two digits
+ * @a: multiplies b
+ * @b: multiplied by a
+ * Return: Always Success
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a *b;
+	return (c);
+}
