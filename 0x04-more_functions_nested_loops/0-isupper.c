05db52e0caa47471543417c5ddca3739351223fc
MikeNdlovu <mickeymusa03@gmail.com>
2023-09-14 07:51:16 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - checks for uppercase letters
+ * @c: letter being checked
+ * Return: 1 for uppercase & 0 otherwise
+ */
+int _isupper(int c)
+{
+
+if (c <= 'A' && c <= 'Z')
+{
+_putchar(c + '0');
+_putchar(':');
+_putchar(' ');
+c++;
+
+return (1);
+_putchar('\n');
+}
+else
+{
+return (0);
+_putchar('\n');
+}
+}
