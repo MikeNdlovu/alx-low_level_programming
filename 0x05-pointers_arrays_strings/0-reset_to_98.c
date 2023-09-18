6c1f17b02dc55e4ee48c997cf1b8db2eb407f41d
MikeNdlovu <mickeymusa03@gmail.com>
2023-09-18 12:56:07 -0700

    mgnf

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,8 @@
 #include "main.h"
-#include "main.h"
 
-/*
- * reset_to_98 - resets int value of pointed var to 98
- * @*n - pointer that is pointing to var
+/**
+ * reset_to_98 - changes int value of pointed var to 98
+ * @n: pointer that is pointing to var
  * Return: 0always
  */
 
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/*
+ * swap_int - swaps 2 int values
+ * @a: swaps to b
+ * @b: swaps to a
+ * Return: 0 always
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+}
