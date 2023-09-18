76fe0e0cb13848d9d589e88c121b152cd4eace81
MikeNdlovu <mickeymusa03@gmail.com>
2023-09-18 12:41:38 -0700

    nhhk

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include "main.h"
+
+/*
+ * reset_to_98 - resets int value of pointed var to 98
+ * @*n - pointer that is pointing to var
+ * Return: 0always
+ */
+
+void reset_to_98(int *n);
+{
+	*n = 92;
+}
