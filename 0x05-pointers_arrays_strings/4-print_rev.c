d7fc6acef1800e210dd1acdf59383b59d567b2ea
MikeNdlovu <mickeymusa03@gmail.com>
2023-09-18 15:40:12 -0700

    kjh

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,7 +7,7 @@
  */
 void print_rev(char *s)
 {
-	int f;
+	int f = 0;
 
 	while (s[f] != 0 && f >=0)
 	{
@@ -15,6 +15,7 @@ void print_rev(char *s)
 	while (f -= 1 && f >= 0)
 	{
 	_putchar(s[f]);
+	f--;
 	}
 	_putchar('\n');
 	}
