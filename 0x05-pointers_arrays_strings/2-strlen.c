b0fe250a6b96729ba47af8258af4d7f3433f3de4
MikeNdlovu <mickeymusa03@gmail.com>
2023-09-18 13:59:49 -0700

    kjh

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - prints the length of a string
+ * @s: string length being printed
+ * Return: 0always
+ */
+
+int _strlen(char *s);
+{
+	int length;
+
+	length = strlen(*s);
+	
+	_putchar(length);
+	_putchar('\n');
+}
