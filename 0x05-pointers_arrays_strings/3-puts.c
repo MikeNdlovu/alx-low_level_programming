c726d9c0fe2dec1432511ee3604448d71e72b8b6
MikeNdlovu <mickeymusa03@gmail.com>
2023-09-18 15:05:28 -0700

    kjh

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints string and new line to stdout
+ * @str - strng being printed
+ * Return: 0always
+ */
+void _puts(char *str)
+{
+	_puts(str);
+	return (0);
+}
