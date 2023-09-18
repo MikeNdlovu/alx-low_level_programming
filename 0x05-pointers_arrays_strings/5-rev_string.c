56604b43208212a1304dccb25e4992760527ae56
MikeNdlovu <mickeymusa03@gmail.com>
2023-09-18 15:58:52 -0700

    kjh

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - reverse string
+ * @s: string being reversed
+ * Return: 0always
+ */
+
+void rev_string(char *s)
+{
+int c, v;
+char wtl;
+}
