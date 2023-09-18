51fdd91c51a9b522188136924346fc39372a84b5
MikeNdlovu <mickeymusa03@gmail.com>
2023-09-18 15:16:06 -0700

    kjh

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,11 +3,15 @@
 
 /**
  * _puts - prints string and new line to stdout
- * @str - strng being printed
+ * @str: strng being printed
  * Return: 0always
  */
 void _puts(char *str)
 {
+	while (*str != '\0')
+	{
 	_putchar(*str);
+	str++;
+	}
 	_putchar('\n');
 }
