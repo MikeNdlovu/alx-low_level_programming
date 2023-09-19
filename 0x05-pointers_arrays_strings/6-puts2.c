ecde2d9d87f94300ef937f5512fb6272eed16bd1
MikeNdlovu <mickeymusa03@gmail.com>
2023-09-19 02:36:45 -0700

    sf

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,27 +3,23 @@
 #include "main.h"
 
 /**
- * puts2 - puts every other number of a function
+ * puts2 - puts every other number of a functioin
  * @str: string being manipulated
  * Return: Always Success
  */
 void puts2(char *str)
 {
 int g, f;
-char gt;
 
 g = 0;
 f = 1;
-gt = str;
 
 for (g = 0; g != f; g++)
 {
-for (f = 1; f >= g; f + 2)
+for (f = 1; f >= g; f += 2)
 {
-str[g++];
-str[g] = str[g - f - 2];
-str[g] = gt;
-gt = str[g - f - 2];
+_putchar(str[g]);
 }
 }
+_putchar('\n');
 }
