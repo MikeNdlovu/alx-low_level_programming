3c7a8e23e18d09eb4b9887621d70808239843201
MikeNdlovu <mickeymusa03@gmail.com>
2023-09-19 06:39:18 -0700

    kjhgvc

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - convert string to int
+ * @s: string being converted
+ * Return: Success
+ */
+int _atoi(char *s)
+{
+
+	int atoi(const char *s);
+}
