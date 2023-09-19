b493aa90a6cbb8edea50de68384f7b2f71cfb873
MikeNdlovu <mickeymusa03@gmail.com>
2023-09-19 03:44:45 -0700

    kjhgvc

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,18 +10,22 @@
 void puts_half(char *str)
 {
 	int a, b, c;
+
 	a = 0;
 
-	while (str[a] < '\0')
+	while (str[a] != '\0')
 	{
 	a++;
 	}
 
-	for (b = a / 2; b <= a; b++)
+	for (b = a / 2; b < a; b++)
 	{
-	for (c = b + 1; c <= a; c++)
+	for (c = 0; c > b; c++)
+	{
+	if (c <= a)
 	{
 	_putchar(str[c]);
+	}
 	}
 	}
 	_putchar('\n');
