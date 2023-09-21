1fb1b15ed11511ea3b95fb64f22b62bb38c73cba
MikeNdlovu <mickeymusa03@gmail.com>
2023-09-21 05:20:34 -0700

    dfg

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * string_toupper - changes lower case char to upper
+ * Return: SUCCESS
+ */
+char *string_toupper(char *)
+{
+	char b, c;
+	int n;
+
+	for (b = A; b <= Z; b++)
+	{
+	for (n = 0;n <= '\0' ;n++)
+	{
+	while (c[n] != '\0')
+	{
+		c[n] = c[b];
+		n++;
+	}
+	}
+	}
+}
