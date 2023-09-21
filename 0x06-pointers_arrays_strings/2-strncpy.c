cfe75fa9160d17aafccc85f71dd5b2730859dff4
MikeNdlovu <mickeymusa03@gmail.com>
2023-09-21 03:10:08 -0700

    kj

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -16,7 +16,7 @@ char *_strncpy(char *dest, char *src, int n)
 	int g;
 
 	b = 0;
-	while (dest[b] != '\0')
+	while (src[b] != '\0')
 	{
 		b++;
 	}
@@ -24,7 +24,7 @@ char *_strncpy(char *dest, char *src, int n)
 	g = 0;
 	while (g < n && src[g] != '\0')
 	{
-		dest[b] = src[g];
+		dest[g] = src[g];
 		b++;
 		g++;
 	}
