a460b685bfa8105a7c54f94776faf729b5a9d097
MikeNdlovu <mickeymusa03@gmail.com>
2023-09-21 03:33:20 -0700

    rfd

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strcmp - compares two strings
+ * @s1: string being compared
+ * @s2: string being compared
+ * Return: 0 if s1=s2; <0 if s1<s2; >0 if s1>s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	strcmp(s1, s2);
+}
