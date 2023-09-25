3a1280b9f654784407860f016d0c24907ecfdb9e
MikeNdlovu <mickeymusa03@gmail.com>
2023-09-25 15:08:22 -0700

    kj

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strspn - unction that gets the length of a prefix substring
+ *@s: returns bytes in s
+ *@accept: bytes taken for accept
+ *Return: Success
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int m;
+
+m = size_t strspn(const char s, const char accept);
+
+return (m);
+}
