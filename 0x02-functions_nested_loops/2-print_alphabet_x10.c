8c5954c35d3fb1daac00688164bac02e621b4a41
MikeNdlovu <mickeymusa03@gmail.com>
2023-09-27 06:25:08 -0700

    kh

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,10 +7,10 @@
  */
 void print_alphabet_x10(void)
 {
-	
+
 int a = '0';
 char b;
-	
+
 while (a <= '9')
 {
 for (b = 'a'; b <= 'z'; b++)
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,16 +3,16 @@
 /**
  * _isalpha - checks char or other
  * @c: variable to be checked
- * Return: Always (Success) 
+ * Return: Always (Success)
 */
 int _isalpha(int c)
 {
 	if (c >= 'a' && c <= 'z')
 	return (1);
-	
+
 	else if (c >= 'A' && c <= 'Z')
 	return (1);
-	
+
 	else
 	return (0);
 }
