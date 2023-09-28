2b13be679045d1e70382a71d1eb844f8a30d44c9
MikeNdlovu <mickeymusa03@gmail.com>
2023-09-28 05:28:49 -0700

    fdvbjkbg

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * _sqrt_recursion - prints squar root of n
+ * @n: number to be rooted
+ * Reurn: success
+ */
+int _sqrt_recursion(int n)
+{
+	if (n % n = 0)
+	{
+		n = n / n
+	}
+
+	else if (n % n != 0)
+	{
+		n = -1;
+	}
+
+	return (n);
+}
