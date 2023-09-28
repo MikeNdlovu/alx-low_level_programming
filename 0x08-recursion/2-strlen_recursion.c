42118d16955f9a1ffa46cee17e5305adc8602c62
MikeNdlovu <mickeymusa03@gmail.com>
2023-09-28 03:21:59 -0700

    fdsg

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen_recursion - prints length of a string
+ * @s: string to be printed
+ * Return: Success
+ */
+int _strlen_recursion(char *s)
+{
+	int n;
+
+	n = strlen(s);
+
+	return (n);
+}
