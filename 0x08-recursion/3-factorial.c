22ebe38a0779b3fc4ffa125f5b02bf9a6e5b6a75
MikeNdlovu <mickeymusa03@gmail.com>
2023-09-28 04:00:19 -0700

    lkjhgr

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -9,14 +9,10 @@
  */
 int factorial(int n)
 {
-	if (n >= 0)
+	if (n < 0)
 	{
-	return (n * factorial(n - 1));
+		return (-1);
 	}
 
-	else if (n < 0)
-	{
-	return (-1);
-	}
-	return (1);
+	return (n * factorial(n - 1));
 }
