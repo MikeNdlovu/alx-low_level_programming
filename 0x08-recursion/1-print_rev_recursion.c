a2d8af5970109322f7ac7a20221daac241036445
MikeNdlovu <mickeymusa03@gmail.com>
2023-09-28 03:13:16 -0700

    fdsg

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -11,7 +11,7 @@
 void _print_rev_recursion(char *s)
 {
 	int len = strlen(s);
-	int i = 0;
+	int i = 1;
 	int middle = len / 2;
 	char c;
 
@@ -19,8 +19,7 @@ void _print_rev_recursion(char *s)
 	if (i < middle)
 	{
 		c = s[i];
-		s[i] = s[len - i - 1 ];
+		s[i] = s[len - i - 1];
 		s[len - i - 1] = c;
-
 	}
 }
