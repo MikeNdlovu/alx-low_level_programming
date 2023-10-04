0d277fe71a9409cffe9a58945d9859352f49d5ac
MikeNdlovu <mickeymusa03@gmail.com>
2023-10-04 13:45:41 -0700

    kkjhgf

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -4,7 +4,7 @@
 #include <string.h>
 
 /**
- * free_grid -  frees a 2 dimensional grid previously created by alloc_grid function
+ * free_grid -  frees a 2 dimensional grid previously created by alloc_grid
  * @grid: pointer to pointer
  * @height: height of the grid
  * Return: success
