dfdfcee5a81f241aa8bd03be23de5a6a2b3c4ff0
MikeNdlovu <mickeymusa03@gmail.com>
2023-10-04 14:16:28 -0700

    kkjhgf

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * strtow - ...
+ * @str: ...
+ * Return: hjg
+ */
+char **strtow(char *str)
+{
+	char **g;
+	int m = 0;
+
+	while (str[m])
+	{
+		m++;
+	}
+
+	g = malloc(sizeof(char *) * m);
+
+	g = &str;
+	*g = str[m];
+
+	return (g);
+}
