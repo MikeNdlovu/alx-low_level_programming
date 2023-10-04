2a715681e1a7f2950ff5bfc0cceb10781b578673
MikeNdlovu <mickeymusa03@gmail.com>
2023-10-04 08:36:25 -0700

    malloc and free

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * str_concat - mixes s1 and s2
+ * @s1: original string
+ * @s2: string to be added on
+ * Return: SUCCESS
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int len;
+	int len2;
+
+	len = strlen(s1);
+	len2 = strlen(s2);
+	s1 = malloc(len * sizeof(char));
+	s2 = malloc(len2 * sizeof(char));
+
+	strcat(s1, s2);
+	return (s1);
+}
