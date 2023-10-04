3c0989d8920045a41a8aa4da321b8e198cf3f457
MikeNdlovu <mickeymusa03@gmail.com>
2023-10-04 11:45:36 -0700

    gsfbd

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,12 +13,32 @@ char *str_concat(char *s1, char *s2)
 {
 	int len;
 	int len2;
+	int i = 0, n = 0;
 
 	len = strlen(s1);
 	len2 = strlen(s2);
+
+	if (s1 == NULL || s2 == NULL)
+	{
+		return (NULL);
+	}
+
 	s1 = malloc(len * sizeof(char));
 	s2 = malloc((len2 * sizeof(char)) + 1);
 
-	strcat(s1, s2);
+	while (i < len)
+	{
+		s1[i] = s1[i];
+		i++;
+	}
+
+	while (n < len2)
+	{
+		s1[n] = s2[n];
+		n++;
+	}
+
+	s1 = '\0';
+
 	return (s1);
 }
