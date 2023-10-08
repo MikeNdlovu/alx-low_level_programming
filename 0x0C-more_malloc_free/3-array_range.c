20ecf99f7f9c85ff25e665308281b31dc5db264d
MikeNdlovu <mickeymusa03@gmail.com>
2023-10-08 14:20:27 -0700

    making funtions using malloc and free

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -16,7 +16,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	mk = malloc(sizeof(int) * (max - min) + sizeof(int));
+	mk = malloc((sizeof(int) * (max - min)) + sizeof(int));
 
 	if (mk == NULL)
 		return (NULL);
