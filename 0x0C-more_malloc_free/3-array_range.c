8f0d4d3fa92714af349643276b1eb80d5cfaa500
MikeNdlovu <mickeymusa03@gmail.com>
2023-10-08 14:16:20 -0700

    gt

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * array_range - pputs range of array
+ * @min: minimun int value
+ * @max: maximum int value
+ * Return: success
+ */
+int *array_range(int min, int max)
+{
+	int *mk, e = 0;
+
+	if (min > max)
+		return (NULL);
+
+	mk = malloc(sizeof(int) * (max - min) + sizeof(int));
+
+	if (mk == NULL)
+		return (NULL);
+
+	while (min <= min)
+	{
+		mk[e] = min;
+		e++;
+		min++;
+	}
+
+	return (mk);
+}
