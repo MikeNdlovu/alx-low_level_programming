8dc62093fc35a0d4237bf51bf8aacd604e0923f6
MikeNdlovu <mickeymusa03@gmail.com>
2023-10-11 12:56:48 -0700

    using struct and typedef

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * new_dog - puts new info for dog
+ * @name: name of new dog
+ * @age: age of new dog
+ * @owner: owner of new dog
+ * Return: success
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	char *s;
+
+	if (s != NULL)
+	{
+		s->name = name;
+		s->age = age;
+		s->owner = owner;
+	}
+	return (s);
+}
