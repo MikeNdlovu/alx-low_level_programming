416e6a62237482c56454f0237a5cdbeb725b8c39
MikeNdlovu <mickeymusa03@gmail.com>
2023-10-11 08:50:12 -0700

    using struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+#include <stdlib.h>
+#endif
+
+/**
+ * struct_dog - struct containing three elements
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
