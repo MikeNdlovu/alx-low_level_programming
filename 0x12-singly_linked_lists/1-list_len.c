8f1811c8572fbc4ab737272590962f8d4f29a5dc
MikeNdlovu <mickeymusa03@gmail.com>
2023-10-19 06:54:20 -0700

    singly linked lists

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -5,3 +5,21 @@
 
 /**
  * list_len - returns number of elements in a list
+ * @h: pointer to list
+ * Return: success
+ */
+size_t list_len(const list_t *h)
+{
+	size_t num = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h)
+	{
+	h = h->next;
+	num++;
+	}
+
+	return (num);
+}
