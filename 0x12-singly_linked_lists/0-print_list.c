f373f1ef4ebca870571c1503853b9ad7ee0e65f7
MikeNdlovu <mickeymusa03@gmail.com>
2023-10-19 06:25:17 -0700

    singly linked functions

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,14 +6,13 @@
 /**
  * print_list - prints all the elements of a list
  * @h: pointer to list to be printed
- * 
  * Return: success
  */
 size_t print_list(const list_t *h)
 {
 	size_t i = 0;
 
-	while(h)
+	while (h)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
