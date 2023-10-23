8b0d30f9abefd0ce8e504ea9e88690e354fbf0f9
MikeNdlovu <mickeymusa03@gmail.com>
2023-10-23 07:21:04 -0700

    more singly linked lists code

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * free_listint - frees list of nodes
+ * @head: pointer to first node
+ * Return: success
+ */
+void free_listint(listint_t *head)
+{
+        listint_t *current;
+
+        if (head == NULL)
+                return;
+        while (head->next != NULL)
+        {
+                current = head->next;
+                free(head);
+                head = current;
+        }
+        free(head);
+}
