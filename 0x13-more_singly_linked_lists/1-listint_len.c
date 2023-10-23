9c78142e4a139196cd08872f331958124018803b
MikeNdlovu <mickeymusa03@gmail.com>
2023-10-23 06:32:28 -0700

    more singly linked lists

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,20 +1,19 @@
 #include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
+#include <string.h>
 
 /**
- * print_listint - prints all the elements of a list
+ * listint_len - writes no. of elements in linked lists(nodes)
  * @h: pointer to list
  * Return: success
  */
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	int i = 0;
 
 	while (h)
 	{
-	printf("%d\n", h->n);
 	h = h->next;
 	i++;
 	}
