518f8a0402dc78b2dd6563d440d55995085983d2
MikeNdlovu <mickeymusa03@gmail.com>
2023-10-24 23:42:45 -0700

    using vardiac functions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,9 +16,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int p = 0;
 	va_list args;
 
-	if (separator == NULL)
-		return;
-
 	va_start(args, n);
 
 	while (p < n && separator != NULL)
