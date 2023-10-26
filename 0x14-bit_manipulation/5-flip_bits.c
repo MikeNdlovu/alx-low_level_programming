5b8d4157a7c6d1a95193fced3638e5632f1931d0
MikeNdlovu <mickeymusa03@gmail.com>
2023-10-26 08:24:51 -0700

    no. of bits it takes to get to new number

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+ * flip_bits - returns no. of bits needed to flip to get to other no.
+ * @n: number 1 being evaluated
+ * @m: number 2 being compared with
+ * Return: success
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int a, b;
+
+	a = n ^ m;
+	for (b = 0; a > 0;)
+	{
+	if((a & 1) == 1)
+	b++;
+	a = a >> 1;
+	}
+	return (b);
+}
