92d8ae90a15bca4d5ad7865b97d706a76b6d9eba
MikeNdlovu <mickeymusa03@gmail.com>
2023-10-26 07:10:53 -0700

    bit manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,19 +2,27 @@
 #include "main.h"
 
 /**
+ * bi - convetrs and prints decimal to binary
+ * @n: decimal being converted
+ * Return: void
  */
 void bi(unsigned long int n)
 {
 
-        if (n == 0)
-       return;
+	if (n == 0)
+	return;
 
-        bi(n >> 1);
-        if ((n & 1) == 1)
-        putchar('1');
-        if ((n & 1) == 0)
-        putchar('0');
+	bi(n >> 1);
+	if ((n & 1) == 1)
+	putchar('1');
+	if ((n & 1) == 0)
+	putchar('0');
 }
+/**
+ * print_binary - prints binary format of decimal
+ * @n: decimal being convreted
+ * Return: void
+ */
 void print_binary(unsigned long int n)
 {
 	if (n == 0)
