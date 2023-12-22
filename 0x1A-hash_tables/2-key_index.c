aa29f7ae9988d4ca823346973cb2295c0eb6f1bd
MikeNdlovu <mickeymusa03@gmail.com>
2023-12-22 13:37:19 -0800

    getting the index

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,6 +1,12 @@
 #include "hash_tables.h"
 
+/**
+ * key_index - gets index of where a key is stored in a list
+ * @key: the key being stored
+ * @size: size of the array
+ * Return: Success
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	return hash_djb2(key) % (size);
+	return (hash_djb2(key) % (size));
 }
